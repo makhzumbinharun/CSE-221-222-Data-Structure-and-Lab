5a0fb20cbcfb97b0491939c45e14aab918c4aeb3
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-05-18 08:58:57 +0600

    Fixed the "NOT FOUND" logic

--- LAB/Lab 01/Linear Search/Linear Search.cpp
+++ LAB/Lab 01/Linear Search/Linear Search.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void linear_search(int arr[], int N, int s){
+    int found = 0;
+	for(int i = 0; i < N; i++){
+	    if(arr[i] == s) cout << s << " IS FOUND IN POSITION " << i+1; found = 0;
+	    if(arr[i] != s) found++;
+	}
+	
+	if(found != 0) cout << "NOT FOUND IN THE ARRAY!" << endl;
+}
+
+
 int main() {
     int N;
     cout << "ENTER THE SIZE OF THE ARRAY: ";
@@ -9,16 +20,12 @@ int main() {
 	for(int i = 0; i < N; i++){
 	    cin >> arr[i];
 	}
+	
 	int s;
 	cout << "Enter the value to search: ";
 	cin >> s;
-	int found = 0;
-	for(int i = 0; i < N; i++){
-	    if(arr[i] != s) found++;
-	    else if(arr[i] == s) cout << s << " IS FOUND IN POSITION " << i+1; found = 0;
-	}
-	
-	if(found != 0) cout << "NOT FOUND IN THE ARRAY!" << endl;
+	linear_search(arr, N, s);
 	
 return 0;
 }
+
