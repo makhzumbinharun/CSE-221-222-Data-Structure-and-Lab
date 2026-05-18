b29eb93bf47d7d6579af9a4d37e07c6669e11be0
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-05-18 08:48:02 +0600

    Create Linear Search.cpp

---
+++ LAB/Lab 01/Linear Search/Linear Search.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "ENTER THE SIZE OF THE ARRAY: ";
+	cin >> N;
+	int arr[N];
+	for(int i = 0; i < N; i++){
+	    cin >> arr[i];
+	}
+	int s;
+	cout << "Enter the value to search: ";
+	cin >> s;
+	int found = 0;
+	for(int i = 0; i < N; i++){
+	    if(arr[i] != s) found++;
+	    else if(arr[i] == s) cout << s << " IS FOUND IN POSITION " << i+1; found = 0;
+	}
+	
+	if(found != 0) cout << "NOT FOUND IN THE ARRAY!" << endl;
+	
+return 0;
+}
