63f0ca31cb7904b8f8b164933669741be9824b3f
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-06-08 14:41:53 +0600

    Create Updation in a String.c

---
+++ LAB/Lab 02/Updation in a String/Updation in a String.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int s;
+    char str[s];
+    char v;
+    int p;
+
+    printf("Enter string size: ");
+    scanf("%d", &s);
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+//    fgets(str, sizeof(str), stdin);
+//    str[strcspn(str, "\n")] = '\0';
+
+    printf("Before Update: %s\n", str);
+
+    int len = strlen(str);
+
+    printf("Enter position and character: ");
+    scanf("%d %c", &p, &v);
+
+    if (p < 0 || p > len) {
+        printf("INVALID!!\n");
+        return 0;
+    }
+    str[p] = v;
+
+    printf("After Update: %s\n", str);
+
+    return 0;
+}
+
