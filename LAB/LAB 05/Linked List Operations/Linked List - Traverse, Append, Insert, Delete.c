2dcfadf69bf33273e36d6846e0e14d57d9df1767
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-07-13 10:20:04 +0600

    Create Linked List - Traverse, Append, Insert, Delete.c

---
+++ LAB/LAB 05/Linked List Operations/Linked List - Traverse, Append, Insert, Delete.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Node Structure
+
+struct Node{
+    int data;
+    struct Node *next;
+}*head = NULL;
+
+
+// Traverse
+
+void traverse(){
+    struct Node *temp = head;
+    
+    if(head == NULL){
+        printf("\nList is Empty!\n");
+        return;
+    }
+    printf("\nLinked List: ");
+    
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+
+// Append
+
+
+void append(int value){
+    struct Node *newNode, *temp;
+    
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    
+    newNode->data = value;
+    newNode->next = NULL;
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    
+    temp = head;
+    
+    while(temp->next != NULL)
+        temp = temp->next;
+    
+    temp->next = newNode;
+}
+
+
+// Insert
+
+void insert(int value, int position){
+    struct Node *newNode, *temp;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    
+    if(position == 1){
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+    
+    temp = head;
+    
+    for(int i = 1; i < position - 1 && temp != NULL; i++)
+        temp = temp->next;
+    
+    if(temp == NULL){
+        printf("Invalid Position!\n");
+        free(newNode);
+        return;
+    }
+    
+    newNode->next = temp->next;
+    temp->next = newNode;
+    
+}
+
+
+// Delete
+
+void deleteNode(int position){
+    struct Node *temp, *prev;
+    
+    if(head == NULL){
+        printf("List is Empty!\n");
+        return;
+    }
+    
+    temp = head;
+    
+    if(position == 1){
+        head = head->next;
+        free(temp);
+        return;
+    }
+    
+    prev = NULL;
+    
+    for(int i = 1; i < position && temp != NULL; i++){
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    if(temp == NULL){
+        printf("Invalid Position!\n");
+        return;
+    }
+    
+    prev->next = temp->next;
+    free(temp);
+    
+}
+
+
+int main(){
+    
+    int choice, value, position;
+    
+    while(1){
+        printf("\n===== LINKED LIST MENU =====\n");
+        printf("1. Append\n");
+        printf("2. Insert\n");
+        printf("3. Delete\n");
+        printf("4. Traverse\n");
+        printf("0. Exit\n");
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+        
+        
+        switch(choice){
+            
+            case 1:
+                printf("Enter Value: ");
+                scanf("%d", &value);
+                append(value);
+                break;
+            
+            
+            case 2:
+                printf("Enter Value: ");
+                scanf("%d", &value);
+                printf("Enter Position: ");
+                scanf("%d", &position);
+                insert(value, position);
+                break;
+            
+            
+            case 3:
+                printf("Enter Position to Delete: ");
+                scanf("%d", &position);
+                deleteNode(position);
+                break;
+            
+            
+            case 4:
+                traverse();
+                break;
+            
+            case 0:
+                printf("Program Ended.\n");
+                exit(0);
+            
+            default:
+                printf("Invalid Choice!\n");
+            
+            
+        }
+    }
+    
+    return 0;
+}
